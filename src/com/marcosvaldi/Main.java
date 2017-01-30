cdf7107ca160628628226ba58bbbd4a91e83d8d5
Markinhos_3\Markinhos3 <hablamos@marcosvaldi.com>
2017-01-30 19:22:04 +0100

    Added welcome_message

--- src/com/marcosvaldi/Main.java
+++ src/com/marcosvaldi/Main.java
@@ -3,6 +3,9 @@ package com.marcosvaldi;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Welcome.printWelcomeMessage();
+
+
     }
 }
---
+++ src/com/marcosvaldi/Welcome.java
@@ -0,0 +1,12 @@
+package com.marcosvaldi;
+
+
+public class Welcome {
+
+    public static void printWelcomeMessage() {
+
+        String title = "-          Agenda 2017 | © Marcos Valdi          -";
+
+        System.out.println("\n" + title.toUpperCase());
+    }
+}
